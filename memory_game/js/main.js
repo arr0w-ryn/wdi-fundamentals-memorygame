7c78f248417df15783bdd2affb686e741420b8f6
Aaron Griffin <aarongriffin@Aarons-MacBook-Pro.local>
2019-01-31 21:18:24 -0800

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped "+cardOne);
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0]===cardsInPlay[1]){
+			alert("You found a match!")
+		}
+		else {alert("Sorry. Try again.")}
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+cardTwo);
-
-if (cardsInPlay.length === 2) {
-	console.log("User flipped 2 cards");
-	if (cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!")
 	}
-	else {alert("Sorry. Try again.")}
 }
+
+var flipCard = function(cardID){
+	console.log("User flipped "+cards[cardID])
+	cardsInPlay.push(cards[cardID])
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
