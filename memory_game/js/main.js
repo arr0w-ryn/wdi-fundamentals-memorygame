ca81a5f485ca7a62f414b6804ab12de741a219f1
Aaron Griffin <aarongriffin@Aarons-MacBook-Pro.local>
2019-02-01 18:56:49 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,31 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		image: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		image: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		image: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		image: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0]===cardsInPlay[1]){
-			alert("You found a match!")
+			alert("You found a match!");
 		}
 		else {alert("Sorry. Try again.")}
 
@@ -12,8 +33,10 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardID){
-	console.log("User flipped "+cards[cardID])
-	cardsInPlay.push(cards[cardID])
+	console.log("User flipped "+cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].image);
+	console.log(cards[cardID].suit);
 	checkForMatch();
 }
 
